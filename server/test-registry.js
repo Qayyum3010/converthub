04a40ab10d69b92d38d326fbcfee883c89b66ea9
Oyedepo Abdulqayyum <oyedepoabdulqayyum2@gmail.com>
2026-08-31 22:32:37 +0100

    chore: remove scratch registry test file

--- server/test-registry.js
+++
@@ -1,5 +0,0 @@
-const { validatePair } = require("./conversions/registry");
-
-console.log("Unknown pair:", validatePair("md", "exe"));
-console.log("Known but unimplemented:", validatePair("md", "html"));
-console.log("With dots in extension:", validatePair(".md", ".html"));
